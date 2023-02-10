9caf1e1f1dcfca1895d643cf0be957a87a61f280
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2023-02-11 00:29:12 +0100

    added some JavaScript codes

---
+++ index.js
@@ -0,0 +1,20 @@
+//function that multiples two numbers
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+let result = multiply(4, 5);
+console.log(result); // Output: 20
+
+
+let  first_name = "Benita"
+let last_name = "Ekene-Okikere"
+let Stutern_Track = "1.4 Cohort"
+let skills = "HTML, CSS, JavaScript, Node.js, Express, MongoDB, mySQL"
+let color = "purple"
+let  independence = "October 1, 1960"
+console.log("My first name is " + first_name + " and my last name is " + last_name + "." + "\n" +
+            "I am a learner at Stutern. My track is " + Stutern_Track + "." + "\n" +
+            "I have the following skill sets: " + skills + "." + "\n" +
+            "My favourite color is " + color + "." + "\n" +
+            "I am a Nigerian. The year of your Nigeria’s independence is " + independence + ".");
